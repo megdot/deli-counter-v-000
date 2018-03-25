8f141f227e8267c4f399c07597f85def089faceb
Megan Dotto <megan.dotto@gmail.com>
2018-03-25 02:08:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,11 @@
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
-  else
-    string = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      string = string + "#{index+1}. #{name}"
-    end
-  puts string
-end
+  else current_line = katz_deli.collect.with_index do |person, i|
+    "#{i+1}. #{person}"
+  end
+    puts "The line is currently: " + current_line.join(" ")
+  end
 end
 
 def take_a_number(katz_deli, new_person)
