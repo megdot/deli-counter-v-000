6028978216188c054ed6e77041758618538fad8c
Megan Dotto <megan.dotto@gmail.com>
2018-03-25 01:51:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 # Write your code here.
-katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    array.each_with_index { |}
+    string = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+      string = string + "#{index+1}. #{name}"
+    end
+    puts string
 end
-
