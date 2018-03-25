7f86ee60295699bde9b4bae28acf267066c6afe0
Megan Dotto <megan.dotto@gmail.com>
2018-03-25 01:52:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 
-def line(katz_deli)
-  if katz_deli.size == 0
+def line(array)
+  if array.size == 0
     puts "The line is currently empty."
   else
     string = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
+    array.each_with_index do |name, index|
       string = string + "#{index+1}. #{name}"
     end
   puts string
